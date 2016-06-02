3d6b32219e12ca8ff82c262138fa4233ee4b84f4
kjl012989 <lee.kevinj@outlook.com>
2016-06-02 18:33:22 -0400

    moved files
    committed files to wrong directory, fixing that by moving all files…

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Speed;
+	private int count;
+	public GUIText countText;
+	public GUIText winText;
+
+	void Start(){
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * Speed * Time.deltaTime);
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count == 12) {
+			winText.text = "YOU WIN!";
+		}
+	}
+
+}
